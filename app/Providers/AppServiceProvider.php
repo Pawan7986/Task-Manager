1c7fd39077912737781ca7d5903eeb39e54dc81d
Pawan7986 <kmr.fam07@gmail.com>
2025-09-04 16:58:50 +0530

    Add auto-migrate on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    { 
+        if(app()->environment('production')) {
+            Artisan::call('migrate', ['--force' => true]);
+        }
     }
 }
